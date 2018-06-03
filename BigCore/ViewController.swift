ebe83d8d87f82fc1fc9a1724954b027b7cf35edc
Anup Sukumaran <anup.sukumaran9@gmail.com>
2018-06-03 13:28:07 +0530

    Initial Commit

---
+++ BigCore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BigCore
+//
+//  Created by Sukumar Anup Sukumaran on 03/06/18.
+//  Copyright © 2018 TechTonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
